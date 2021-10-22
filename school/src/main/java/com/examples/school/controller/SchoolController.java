41f06bd4cfaa7b0f803576d78f826783dd3c4e4f
Damiano Giani <damiano.giani@gmail.com>
2021-10-22 09:45:34 +0200

    test

--- school/src/main/java/com/examples/school/controller/SchoolController.java
+++ school/src/main/java/com/examples/school/controller/SchoolController.java
@@ -20,14 +20,15 @@ public class SchoolController {
 	public void newStudent(Student student) {
 		Student existingStudent = studentRepository.findById(student.getId());
 		if (existingStudent != null) {
-			studentView.showError("Already existing student with id" + student.getId(), existingStudent);
+			studentView.showError("Already existing student with id " + student.getId(), existingStudent);
+			return;
 		}
 		studentRepository.save(student);
 		studentView.studentAdded(student);
 	}
 
 	public void deleteStudent(Student student) {
-		if (studentRepository.findById(student.getId()) != null) {
+		if (studentRepository.findById(student.getId()) == null) {
 			studentView.showError("No existing student with id " + student.getId(), student);
 			return;
 		}
